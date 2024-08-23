174fa92a91db1fd04e2fb948fb94789753403a74
yalma-tgh <fateme0ft0@gmail.com>
2024-08-23 13:17:40 +0330

    feat(pingpong_api):add api views

--- pingpong_api/views.py
+++ pingpong_api/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+class PingPongView(APIView):
+    def get(self, request):
+        return Response({"message": "pong"}, status=status.HTTP_200_OK)
