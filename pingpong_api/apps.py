59ebfb63d4f5d45dba61b552c6802e64fe1f64bb
yalma-tgh <fateme0ft0@gmail.com>
2024-08-23 13:04:40 +0330

    feat(pingpong_api):creat pinppong_api and update INSTALLED_APPS

---
+++ pingpong_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PingpongApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'pingpong_api'
--- pingpong_core/settings.py
+++ pingpong_core/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+    'rest_framework',
+    'pingpong']
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
